bc6c83aed29181ba9adfe66e8cd7d2229037f18e
AydanBayramova <bayramoffaaydan1@gmail.com>
2026-06-18 17:15:48 +0400

    Eureka server and gateway

---
+++ api-gateway/src/test/java/az/finalproject/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package az.finalproject.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
